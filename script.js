8ff54c591b29e772ce235418d1ec75dad897185b
Ujjwalc7 <ujjwalkisku14@gmail.com>
2026-06-18 23:43:09 +0530

    frist commit

---
+++ script.js
@@ -0,0 +1,77 @@
+const formDiv = document.querySelector(".form");
+const addBtn = document.querySelector("#addBtn");
+const closeBtn = document.querySelector(".closeBtn");
+const taskList = document.querySelector("#taskList");
+const form = document.querySelector("form");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+const ui = (obj) => {
+  taskList.innerHTML = "";
+  tasks.forEach((element) => {
+    taskList.innerHTML += `<div class="task-card">
+  <div class="task-content">
+    <h3 class="task-title">${element.title}</h3>
+
+    <span class="task-category">${element.category}</span>
+  </div>
+
+  <div class="task-actions">
+    <button class="status-btn" onclick = "completeTask('${element.id}')">
+      ${element.status === "Active" ? "Mark as complete" : "Completed"}
+    </button>
+
+    <button class="delete-btn" onclick = "deleteTask('${element.id}')" >
+      Delete
+    </button>
+  </div>
+</div>`;
+  });
+};
+
+addBtn.addEventListener("click", () => {
+  formDiv.style.display = "flex";
+});
+closeBtn.addEventListener("click", () => {
+  formDiv.style.display = "none";
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = e.target[0].value.trim();
+  const category = e.target[1].value.trim();
+  if (title.trim() === "" || category.trim() === "") {
+    alert("all fields are required");
+    return;
+  }
+  const id = `${Date.now()}-${Math.floor(Math.random() * 1000)}`;
+  const status = "Active";
+  const obj = {
+    id,
+    title,
+    category,
+    status,
+  };
+  tasks.push(obj);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  ui();
+  form.reset();
+  formDiv.style.display = "none";
+});
+
+const completeTask = (id) => {
+  tasks.forEach((ele) => {
+    if (ele.id === id) {
+      ele.status = "Completed";
+      return;
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  ui();
+};
+const deleteTask = (id) => {
+  tasks = tasks.filter((ele) => ele.id !== id);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  ui();
+};
+
+ui();
